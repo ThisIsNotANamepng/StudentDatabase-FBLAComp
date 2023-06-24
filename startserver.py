278ffa0458b006e6983628779c8ea0990060deff
Jack Hagen <TurtleDinasour@protonmail.com>
2023-06-24 10:14:52 -0500

    Added second before printing ip and + alias to DF

--- startserver.py
+++ startserver.py
@@ -4,7 +4,7 @@ import time
 print("Starting server...")
 process = subprocess.Popen(['nohup', 'fbla/bin/python3', 'main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-time.sleep(2)
+time.sleep(4)
 f=open("ip.txt", "r")
 print("Server started at "+f.read())
 f.close()
