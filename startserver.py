0123354e2263db2d93be21da52599085903a470a
Jack Hagen <TurtleDinasour@protonmail.com>
2023-06-24 10:21:05 -0500

    Fixed python3, image should work now

--- startserver.py
+++ startserver.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 print("Starting server...")
-process = subprocess.Popen(['nohup', 'fbla/bin/python3', 'main.py'])
+process = subprocess.Popen(['nohup', 'python3', 'main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 time.sleep(4)
 f=open("ip.txt", "r")
